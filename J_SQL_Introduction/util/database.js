5e127444a92d86333315f9e7ba2415095dbd39a1
James Privett <jamesprivett29@gmail.com>
2024-01-08 14:29:01 +0000

    Connected app to MySQL db

---
+++ J_SQL_Introduction/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'cr00ked29',
+});
+
+module.exports = pool.promise();
