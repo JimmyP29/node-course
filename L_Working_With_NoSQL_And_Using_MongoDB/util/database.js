853a332af5cfe147f8c97581cbdc17e3668becb0
James Privett <jamesprivett29@gmail.com>
2024-01-10 16:56:55 +0000

    Finishing the db connection

--- L_Working_With_NoSQL_And_Using_MongoDB/util/database.js
+++ L_Working_With_NoSQL_And_Using_MongoDB/util/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
     MongoClient
         .connect('mongodb+srv://jamesprivett29:eyontAaJWsEpUPRU@cluster0.vvibpra.mongodb.net/?retryWrites=true&w=majority')
-        .then(result => {
+        .then(client => {
             console.log('Connected!!!');
-            callback(result);
+            _db = client.db();
+            callback();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
