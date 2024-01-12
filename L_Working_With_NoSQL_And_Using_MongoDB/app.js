08397a60aec918d15c42dfb4e0b9fdd6c54f7d84
James Privett <jamesprivett29@gmail.com>
2024-01-12 16:32:01 +0000

    Creating new users

--- L_Working_With_NoSQL_And_Using_MongoDB/app.js
+++ L_Working_With_NoSQL_And_Using_MongoDB/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
+    User.findById('65a1686f7f5fd32fc4340c61')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
     next();
 });
 
