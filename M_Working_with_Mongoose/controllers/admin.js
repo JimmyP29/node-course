f0f414766457346f246056dd2d6ccf8c78d75ca7
James Privett <jamesprivett29@gmail.com>
2024-01-24 15:42:22 +0000

    Relations, an important note - using select() and populate() on find()

--- M_Working_with_Mongoose/controllers/admin.js
+++ M_Working_with_Mongoose/controllers/admin.js
@@ -79,7 +79,10 @@ exports.postEditProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   Product
     .find()
+    // .select('title price -_id') // Selecting which fields we want back and explicitly DON'T want back (-)
+    // .populate('userId', 'name') // This can also point to nested paths - retrieves the entire user object
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
