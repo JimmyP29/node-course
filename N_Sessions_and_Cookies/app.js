f70d661f34aaaa04b94f9eac805a96842d3f9c30
James Privett <jamesprivett29@gmail.com>
2024-01-26 13:27:05 +0000

    Initialising the Session middleware

--- N_Sessions_and_Cookies/app.js
+++ N_Sessions_and_Cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -19,6 +20,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+}));
 
 app.use((req, res, next) => {
     User.findById('65b12acb78f039a9f0d20eea')
