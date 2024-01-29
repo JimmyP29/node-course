651e6ca30f80cfeff0439742d4a28c4f0e379c6d
James Privett <jamesprivett29@gmail.com>
2024-01-29 10:56:57 +0000

    Adding Signup page and hooking up to the routing and controllers

--- O_Adding_Authentication/app.js
+++ O_Adding_Authentication/app.js
@@ -55,21 +55,6 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
-        User.findOne()
-            .then(user => {
-                if (!user) {
-                    const user = new User({
-                        name: 'James',
-                        email: 'test@test.com',
-                        cart: {
-                            items: []
-                        }
-                    });
-
-                    user.save();
-                }
-            });
-
         app.listen(3000);
     })
     .catch(err => console.log(err));
