fd9befadd2cead985237394f33e9c52f1251ebf5
James Privett <jamesprivett29@gmail.com>
2024-01-29 15:13:53 +0000

    Initial Commit

---
+++ R_Understanding_Validation/controllers/admin.js
@@ -0,0 +1,108 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+    userId: req.user // Mongoose is able to assign the user._id by just assigning the whole user object.
+  });
+
+  product
+    .save()
+    .then(result => {
+      //console.log(result)
+      console.log('Created Product');
+      return res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    //Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  Product.findById(prodId)
+    .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+
+      return product.save()
+        .then(() => {
+          console.log('Updated Product');
+          res.redirect('/admin/products');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product
+    .find({ userId: req.user._id })
+    // .select('title price -_id') // Selecting which fields we want back and explicitly DON'T want back (-)
+    // .populate('userId', 'name') // This can also point to nested paths - retrieves the entire user object
+    .then(products => {
+      // console.log(products);
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
+    .then(() => {
+      console.log('Destroyed Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};
