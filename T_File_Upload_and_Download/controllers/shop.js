3f79e3c5bf90b33d3d53767706f94f52c766b4ff
James Privett <jamesprivett29@gmail.com>
2024-01-31 15:21:40 +0000

    Setting file headers

--- T_File_Upload_and_Download/controllers/shop.js
+++ T_File_Upload_and_Download/controllers/shop.js
@@ -165,6 +165,8 @@ exports.getInvoice = (req, res, next) => {
       return next(err);
     }
 
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename:="' + invoiceName + '"');
     res.send(data);
   });
 }
