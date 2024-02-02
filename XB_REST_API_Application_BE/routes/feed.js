8e1c70c541b05f20870f3d885d20a99e7009684e
James Privett <jamesprivett29@gmail.com>
2024-02-02 16:27:06 +0000

    Updating posts

--- XB_REST_API_Application_BE/routes/feed.js
+++ XB_REST_API_Application_BE/routes/feed.js
@@ -17,6 +17,15 @@ router.post('/post', [
         .isLength({ min: 5 }),
 ], feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost)
+router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title')
+        .trim()
+        .isLength({ min: 5 }),
+    body('content')
+        .trim()
+        .isLength({ min: 5 }),
+], feedController.updatePost);
 
 module.exports = router;
