123f7a57cc3835d6fb19ffee76c205a4026c9ad5
James Privett <jamesprivett29@gmail.com>
2024-02-02 15:51:02 +0000

    Fetching a single post

--- XB_REST_API_Application_BE/controllers/feed.js
+++ XB_REST_API_Application_BE/controllers/feed.js
@@ -3,21 +3,17 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res
-        .status(200)
-        .json({
-            posts: [
-                {
-                    _id: '1',
-                    title: 'First Post',
-                    content: 'This is the first post.',
-                    imageUrl: 'images/me.jpeg',
-                    creator: {
-                        name: 'James'
-                    },
-                    createdAt: new Date()
-                }
-            ]
+    Post
+        .find()
+        .then(posts => {
+            res.status(200).json({ message: 'Posts fetched successfully.', posts: posts });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+
+            next(err);
         });
 }
 
@@ -49,6 +45,28 @@ exports.createPost = (req, res, next) => {
                     message: 'Post created successfully.',
                     post: result
                 });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+
+            next(err);
+        });
+}
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post
+        .findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error; // If you throw an error in a then block then it will be passed through as the err in the catch block
+            }
+
+            res.status(200).json({ message: 'Post fetched.', post: post });
         })
         .catch(err => {
             if (!err.statusCode) {
